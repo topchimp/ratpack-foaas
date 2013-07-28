48a199f0680ad957dde3c7834445325eacdc3d8d
Luke Daley <ld@ldaley.com>
2013-07-28 20:10:26 +0100

    Some tests.

---
+++ src/test/groovy/app/FunctionalSpec.groovy
@@ -0,0 +1,40 @@
+package app
+
+import org.ratpackframework.test.ScriptAppSpec
+
+class FunctionalSpec extends ScriptAppSpec {
+
+    def "content negotiation"() {
+        when:
+        request.header("Accept", "*/*")
+
+        then:
+        getText("off/to/from") == "Fuck off, to. - from"
+
+        when:
+        request.header("Accept", "application/json")
+
+        then:
+        with(get("off/to/from").jsonPath()) {
+            get("message") == "Fuck off, to."
+            get("subtitle") == "- from"
+        }
+
+        when:
+        request.header("Accept", "text/html")
+
+        then:
+        with(get("off/to/from")) {
+            contentType == "text/html;charset=UTF-8"
+            body.asString().contains "Fuck off, to."
+        }
+    }
+
+    def "handles unknowns"() {
+        when:
+        get("foo/bar/baasdfa")
+
+        then:
+        response.statusCode == 404
+    }
+}
