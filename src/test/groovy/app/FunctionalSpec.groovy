9f51f04e03da751979d8487db82a80af311505c9
Marco Vermeulen <vermeulen.mp@gmail.com>
2013-08-06 21:23:08 +0100

    Major version now feature complete with Bieber.

--- src/test/groovy/app/FunctionalSpec.groovy
+++ src/test/groovy/app/FunctionalSpec.groovy
@@ -62,5 +62,6 @@ class FunctionalSpec extends ScriptAppSpec {
 		"keepcalm/to/from"     | "Keep Calm to and Fuck Off - from"
 		"steviewonder/to/from" | "I just called, to say..........to FUCK OFF! - from"
 		"malcolmtucker/from"   | "I will tear your fucking skin off, I will wear it to your mother's birthday party, and rub your nuts up and down her leg whilst whistling Bohemian fucking Rhapsody. Right? - from"
+		"justinbieber/from"    | "There's gonna be times in your life when people say you can't do something. And there's gonna be times in your life when people say that you can't live your dreams. This is what I tell them: FUCK OFF! - from"
 	}
 }
