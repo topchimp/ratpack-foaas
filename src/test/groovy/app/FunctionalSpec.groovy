38bc88cd15e6d427b404784055afbd52c560721a
Sean Gilligan <sean@msgilligan.com>
2013-08-06 16:36:54 -0700

    Add "Arthur" (Life of Brian) variant text/doc/test

--- src/test/groovy/app/FunctionalSpec.groovy
+++ src/test/groovy/app/FunctionalSpec.groovy
@@ -64,5 +64,6 @@ class FunctionalSpec extends ScriptAppSpec {
 		"malcolmtucker/from"   | "I will tear your fucking skin off, I will wear it to your mother's birthday party and rub your nuts up and down her leg whilst whistling Bohemian fucking Rhapsody. Right? - from"
 		"justinbieber/from"    | "There's gonna be times in your life when people say you can't do something. And there's gonna be times in your life when people say that you can't live your dreams. This is what I tell them: FUCK OFF! - from"
 		"swearengen/E.B./Al"   | "I will profane your fucking remains, E.B.! - Al"
-	}
+        "arthur/to/from"       | "How shall to fuck off, oh Lord? - from"
+    }
 }
